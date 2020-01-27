a0f8d253c284bde4f4eb63a9780a4299b92ceaf4
mfreydl <letmeaskmydog@gmail.com>
2020-01-27 09:38:09 -0600

    Initialized as a Codiac Api

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "reflect-metadata";
+import { toyhauler } from "@mfreydl/toyhauler-api-framework";
+import { myApiDef } from "./myApiDef";
+
+toyhauler.run(myApiDef);
+        
---
+++ src/myApiDef.ts
@@ -0,0 +1,20 @@
+import { apiDef, apiHost } from "@mfreydl/toyhauler-api-framework";
+
+export class myApiDef extends apiDef {
+
+    /**
+     *
+     */
+    constructor() {
+        super();
+
+    }
+
+    defineEndpoints(host: apiHost): void {
+        
+    }
+
+    Bootstrap(host: apiHost): void {
+       
+    }
+}
